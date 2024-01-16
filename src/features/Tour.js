d4305589652623b92a9a3e011ad21b6c32a41e5e
Manny Vera <vallesvera.manuel@gmail.com>
2024-01-16 00:35:55 -0800

    changed step button color

--- src/features/Tour.js
+++ src/features/Tour.js
@@ -4,7 +4,7 @@ export const steps = [
         id: 'titleStep',
         title: '<h3>Title it!</h3>',
         text: "<p style='color:white;'>Add a title to your note here.</p>",
-        
+
         attachTo: {
             element: '#titleArea',
             on: 'right'
@@ -13,7 +13,7 @@ export const steps = [
         classes: 'tour-content',
         buttons: [
             {
-                // classes : 'shepherd-button-primary',
+                classes: 'shepherd-button-secondary',
                 text: 'Next',
                 type: 'next'
             }
@@ -31,10 +31,11 @@ export const steps = [
         classes: 'tour-content',
         buttons: [
             {
+                classes: 'shepherd-button-secondary',
                 text: 'Next',
                 type: 'next'
             }
-        ] 
+        ]
     },
     {
         id: 'postItStep',
@@ -47,13 +48,14 @@ export const steps = [
         classes: 'tour-content',
         buttons: [
             {
+                classes: 'shepherd-button-secondary',
                 text: 'Next',
                 type: 'next'
             }
         ]
     },
     {
-        title : '<h3>Update It!</h3>',
+        title: '<h3>Update It!</h3>',
         id: 'updateStep',
         text: "<p style='color:white;'>Click the post-it body to update it's content.</p>",
         attachTo: {
@@ -64,13 +66,14 @@ export const steps = [
         classes: 'tour-content',
         buttons: [
             {
+                classes: 'shepherd-button-secondary',
                 text: 'Next',
                 type: 'next'
             }
         ]
     },
     {
-        title : "<h3>Delete It!</h3>",
+        title: "<h3>Delete It!</h3>",
         id: 'deleteStep',
         text: "<p style='color:white;'>Did you make a mistake? No worries! Click the RED PIN to delete your post.</p>",
         attachTo: {
@@ -81,6 +84,7 @@ export const steps = [
         classes: 'tour-content',
         buttons: [
             {
+                classes: 'shepherd-button-secondary',
                 text: 'Next',
                 type: 'next'
             }
